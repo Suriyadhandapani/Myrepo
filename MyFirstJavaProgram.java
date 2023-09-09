6d384591f91c7d335b8ef063a8ad48d9697778c9
Suriyadhandapani <108269092+Suriyadhandapani@users.noreply.github.com>
2023-09-09 16:36:36 +0530

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,25 @@
+package firstproject;
+
+public class MyFirstJavaProgram {
+	public static void main(String[] args)
+	{
+	
+		System.out.println("Integers: " + Integer.SIZE);
+		System.out.println("Integers bytes: " + Integer.SIZE/8);
+		System.out.println("Integers max range " + Integer.MAX_VALUE);
+		System.out.println("Integers MIN RANGE: " + Integer.MIN_VALUE);
+		
+		
+		System.out.println("Double bits: " + Double.SIZE);
+		System.out.println("Double bytes: " + Double.SIZE/8);
+		System.out.println("Double max range: " + Double.MAX_VALUE);
+		System.out.println("Double min range: " + Double.MIN_VALUE);
+		
+		System.out.println("Byte bits: " + Byte.SIZE);
+		System.out.println("Byte bytes: " + Byte.SIZE/8);
+		System.out.println("Byte max range: " + Byte.MAX_VALUE);
+		System.out.println("Byte min range: " + Byte.MIN_VALUE);
+	}
+	
+
+}
